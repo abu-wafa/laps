396b7ab35d96660785d156bcb0ec2c7ae7d18f4d
abdelslam ahmed <abuelwafaabdelsalam@gmail.com>
2021-08-20 01:22:19 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route ,Switch ,Redirect} from 'react-router-dom'
+import Home from './component/Home';
+import Aboutus from './component/Aboutus'
+import Contactus from './component/Contactus';
+import SignUp from './component/Signup';
+import Login from './component/Login';
+import Gallery from './component/Gallery';
+import Cart from './component/Cart';
+import Checkout from './component/Checkout'
+//import './App.css';
 
 function App() {
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+     
+     <Switch>
+      <Route path='/Home' component={Home}/>
+      <Route path='/AboutUs' component={Aboutus}/>
+      <Route path='/ContactUs' component={Contactus}/>
+      <Route path='/SignUp' component={SignUp}/>
+      <Route path='/LogIn' component={Login}/>
+      <Route path="/Gallery" component={Gallery}/>
+      <Route path="/Cart" component={Cart}/>
+      <Route path="/Checkout" component={Checkout}/>
+      <Redirect from="/" to="/Home"/>
+     </Switch>
   );
 }
 
